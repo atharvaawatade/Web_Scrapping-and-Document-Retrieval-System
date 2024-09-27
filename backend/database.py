665fefcec2fd48a02c4bd688c4f6ea767d8638ed
atharvaawatade <97244560+atharvaawatade@users.noreply.github.com>
2024-09-27 09:46:19 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from pymongo import TEXT
 mongo = PyMongo()
 
 def init_db(app):
-    app.config["MONGO_URI"] = "mongodb+srv://atharva2021:123@cluster0.so5reec.mongodb.net/document_retrieval"
+    app.config["MONGO_URI"] = " "
     mongo.init_app(app)
     with app.app_context():
         mongo.db.documents.create_index([("content", TEXT)])
